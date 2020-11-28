df2184c8f8d911c875bc009b66e312c3e9b66672
Daniel <danielkvist@protonmail.com>
2020-11-28 15:36:50 +0100

    feat(app): ask, save and use user's twitter creds

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
+const Configstore = require('configstore');
+const twit = require('twit');
+
+const pkg = require('./package.json');
+const inquirer = require('./lib/inquirer');
+
+const conf = new Configstore(pkg.name);
 
 const welcomeMsg = () => {
 	console.log(
@@ -14,11 +21,65 @@ const welcomeMsg = () => {
 			'    The CLI that allows you to tweet without leaving the terminal!'
 		)
 	);
+
+	console.log('');
 };
 
-const init = () => {
+const successfulTweet = (id) => {
+	console.log(chalk.green(`tweet ${id} successfully published!`));
+	console.log(' ');
+};
+
+const credentials = async () => {
+	let auth = {
+		consumer_key: '',
+		consumer_secret: '',
+		access_token: '',
+		access_token_secret: '',
+	};
+
+	// Check if user has already enter the information needed
+	if (
+		conf.has('consumer_key') &&
+		conf.has('consumer_secret') &&
+		conf.has('access_token') &&
+		conf.has('access_token_secret')
+	) {
+		auth = { ...conf.all };
+	} else {
+		const credentials = await inquirer.askTwitterCreds();
+
+		// Load credentials received by the user
+		auth = { ...credentials };
+
+		// Save credentials
+		conf.set({ ...auth });
+	}
+
+	return auth;
+};
+
+const init = async () => {
 	clear();
 	welcomeMsg();
+	const auth = await credentials();
+
+	const client = new twit({
+		...auth,
+		timeout_ms: 60 * 1000,
+		strictSSL: true,
+	});
+
+	const { tweet } = await inquirer.askTweet();
+
+	client.post('statuses/update', { status: tweet }, (err, data, _response) => {
+		if (err) {
+			console.log(err);
+			return;
+		}
+
+		successfulTweet(data.id_str);
+	});
 };
 
 init();
